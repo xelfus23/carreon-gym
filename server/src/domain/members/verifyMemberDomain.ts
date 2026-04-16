dab5b68d8464612841b4882eb551042c1baa6fb7
pataa <xelfus.dev@gmail.com>
2026-04-16 13:48:54 +0800

    update fixed member verification

---
+++ server/src/domain/members/verifyMemberDomain.ts
@@ -0,0 +1,37 @@
+import { stat } from "node:fs";
+import pool from "../../config/pool.ts";
+
+export const verifyMemberDomain = async (userId: number) => {
+    const result = await pool.query(
+        `
+        SELECT verified FROM users
+        WHERE id = $1`,
+        [userId],
+    );
+
+    const status = result.rows[0];
+
+    console.log(status);
+
+    const res = await pool.query(
+        status.verified
+            ? `
+            UPDATE users 
+            SET verified = false, updated_at = CURRENT_TIMESTAMP 
+            WHERE id = $1 
+            RETURNING id, username, email, verified
+            `
+            : `
+            UPDATE users 
+            SET verified = true, updated_at = CURRENT_TIMESTAMP 
+            WHERE id = $1 
+            RETURNING id, username, email, verified`,
+        [userId],
+    );
+
+    if (res.rowCount === 0) {
+        throw new Error("User not found");
+    }
+
+    return res.rows[0];
+};
