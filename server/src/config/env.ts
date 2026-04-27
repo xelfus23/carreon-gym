0edb248b32860abacaa48a183134cffe047a88be
pataa <xelfus.dev@gmail.com>
2026-04-27 14:26:21 +0800

    updates

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,48 +1,49 @@
-// src/config/env.ts
 import "dotenv/config";
 
 function requireEnv(key: string): string {
-    const value = process.env[key];
-    if (!value) {
-        throw new Error(`❌ Missing environment variable: ${key}`);
-    }
-    return value;
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`❌ Missing environment variable: ${key}`);
+  }
+  return value;
 }
 
 export const env = {
-    NODE_ENV: process.env.NODE_ENV ?? "development",
+  NODE_ENV: process.env.NODE_ENV ?? "development",
 
-    PORT: Number(process.env.PORT ?? 3000),
-    PROVIDER: requireEnv("AI_PROVIDER"),
+  PORT: Number(process.env.PORT ?? 3000),
+  PROVIDER: requireEnv("AI_PROVIDER"),
 
-    // Database
-    // DATABASE_URL: requireEnv("DATABASE_URL"),
-    DATABASE_NAME: requireEnv("DATABASE_NAME"),
-    DATABASE_USER: requireEnv("DATABASE_USER"),
-    DATABASE_PASSWORD: requireEnv("DATABASE_PASSWORD"),
-    DATABASE_HOST: requireEnv("DATABASE_HOST"),
-    DATABASE_PORT: requireEnv("DATABASE_PORT"),
+  // Database
+  // DATABASE_URL: requireEnv("DATABASE_URL"),
+  DATABASE_NAME: requireEnv("DATABASE_NAME"),
+  DATABASE_USER: requireEnv("DATABASE_USER"),
+  DATABASE_PASSWORD: requireEnv("DATABASE_PASSWORD"),
+  DATABASE_HOST: requireEnv("DATABASE_HOST"),
+  DATABASE_PORT: requireEnv("DATABASE_PORT"),
 
 
-    //CORS
-    CORS_ORIGIN: requireEnv("CORS_ORIGIN"),
-    // Auth
-    JWT_ACCESS_SECRET: requireEnv("JWT_ACCESS_SECRET"),
-    JWT_REFRESH_SECRET: requireEnv("JWT_REFRESH_SECRET"),
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? "7d",
-    GYM_QR_SECRET: requireEnv("GYM_QR_SECRET"),
-    QR_EXPIRY_SECONDS: requireEnv("QR_EXPIRY_SECONDS"),
+  //CORS
+  CORS_ORIGIN: requireEnv("CORS_ORIGIN"),
 
-    RESEND_API_KEY: requireEnv("RESEND_API_KEY"),
-    EMAIL_FROM: requireEnv("EMAIL_FROM"),
-    EMAIL_PROVIDER: requireEnv("EMAIL_PROVIDER"),
-    // AWS S3
-    // AWS_REGION: requireEnv("AWS_REGION"),
-    AWS_ACCESS_KEY_ID: requireEnv("AWS_ACCESS_KEY_ID"),
-    AWS_SECRET_ACCESS_KEY: requireEnv("AWS_SECRET_ACCESS_KEY"),
-    AWS_S3_BUCKET: requireEnv("AWS_BUCKET_NAME"),
+  JWT_ACCESS_SECRET: requireEnv("JWT_ACCESS_SECRET"),
+  JWT_REFRESH_SECRET: requireEnv("JWT_REFRESH_SECRET"),
+  JWT_REFRESH_EXPIRES_IN: requireEnv("JWT_REFRESH_EXPIRES_IN") as string,
+  JWT_ACCESS_EXPIRES_IN: requireEnv("JWT_ACCESS_EXPIRES_IN") as string,
 
-    // AI
-    LMSTUDIO_BASE_URL: process.env.LMSTUDIO_BASE_URL ?? "http://localhost:1234",
-    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY ?? "",
+  GYM_QR_SECRET: requireEnv("GYM_QR_SECRET"),
+  QR_EXPIRY_SECONDS: requireEnv("QR_EXPIRY_SECONDS"),
+
+  RESEND_API_KEY: requireEnv("RESEND_API_KEY"),
+  EMAIL_FROM: requireEnv("EMAIL_FROM"),
+  EMAIL_PROVIDER: requireEnv("EMAIL_PROVIDER"),
+  
+  // AWS S3
+  AWS_ACCESS_KEY_ID: requireEnv("AWS_ACCESS_KEY_ID"),
+  AWS_SECRET_ACCESS_KEY: requireEnv("AWS_SECRET_ACCESS_KEY"),
+  AWS_S3_BUCKET: requireEnv("AWS_BUCKET_NAME"),
+
+  // AI
+  LMSTUDIO_BASE_URL: requireEnv("LMSTUDIO_BASE_URL") ?? "http://localhost:1234",
+  GOOGLE_API_KEY: requireEnv("GOOGLE_API_KEY") ?? "",
 };
--- server/src/utils/generateTokens.ts
+++ server/src/utils/generateTokens.ts
@@ -1,35 +1,35 @@
-import jwt from "jsonwebtoken";
+import jwt, { type SignOptions } from "jsonwebtoken";
 import { env } from "../config/env.ts";
 import { AppError } from "./appError.ts";
 
 export const generateTokens = ({
-    sub,
-    role,
+  sub,
+  role,
 }: {
-    sub: number;
-    role: "member" | "admin";
+  sub: number;
+  role: "member" | "admin";
 }) => {
-    const accessToken = jwt.sign(
-        {
-            sub,
-            role,
-        },
-        env.JWT_ACCESS_SECRET!,
-        {
-            expiresIn: "15m",
-        },
-    );
+  const accessToken = jwt.sign(
+    {
+      sub,
+      role,
+    },
+    env.JWT_ACCESS_SECRET!,
+    {
+      expiresIn: env.JWT_ACCESS_EXPIRES_IN as any,
+    },
+  );
 
-    const refreshToken = jwt.sign(
-        {
-            sub,
-            role,
-        },
-        env.JWT_REFRESH_SECRET!,
-        {
-            expiresIn: "7d",
-        },
-    );
+  const refreshToken = jwt.sign(
+    {
+      sub,
+      role,
+    },
+    env.JWT_REFRESH_SECRET!,
+    {
+      expiresIn: env.JWT_REFRESH_EXPIRES_IN as any,
+    },
+  );
 
-    return { accessToken, refreshToken };
+  return { accessToken, refreshToken };
 };
